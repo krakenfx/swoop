fd2fd180fe76cae59051ab5a2682d989277333da
Andreas Brekken <andreas@kraken.com>
2015-08-07 16:25:40 +0200

    Add 30 sec. countdown on blockchain link. close #1

--- src/components/Sent.jsx
+++ src/components/Sent.jsx
@@ -2,7 +2,25 @@ import React, { Component, PropTypes } from 'react';
 
 export default class Sent extends Component {
   static propTypes = {
-    hash: PropTypes.string,
+    hash: PropTypes.string.isRequired,
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      const secondsRemaining = this.state.secondsRemaining - 1;
+      this.setState({ secondsRemaining });
+      if (!secondsRemaining) {
+        clearInterval(this.timer);
+        this.timer = null;
+      }
+    }, 1e3);
+  }
+
+  componentWillUnmount() {
+    if (this.timer) {
+      clearInterval(this.timer);
+      this.timer = null;
+    }
   }
 
   render() {
@@ -18,11 +36,23 @@ export default class Sent extends Component {
         </p>
 
         <p>
-          <a className="button" href={`https://etherchain.org/tx/${this.props.hash}`} target="_blank">
+          <a
+            className="button"
+            href={`https://etherchain.org/tx/${this.props.hash}`}
+            target="_blank"
+            disabled={this.state.secondsRemaining > 0}
+          >
             View in block explorer
+            {this.state.secondsRemaining > 0 && <span>
+              {' '}({this.state.secondsRemaining})
+            </span>}
           </a>
         </p>
       </div>
     );
+  }
+
+  state = {
+    secondsRemaining: 30,
   }
 }
