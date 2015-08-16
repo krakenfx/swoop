474947551ecd5f34db7bdb9799d5cfac70b55bae
Andreas Brekken <andreas@kraken.com>
2015-08-16 15:54:27 +0200

    Fix deposit test.

--- tests/deposit.js
+++ tests/deposit.js
@@ -5,6 +5,6 @@ test('deposit', t => {
   t.plan(1);
 
   t.throws(() => {
-    deposit('0x1234', 'XE83ETHBAKEN1234');
+    deposit('0x1234', 'XE74ETHKRABEN0003088');
   }, /institution/i, 'should reject institution other than KRAKEN');
 });
